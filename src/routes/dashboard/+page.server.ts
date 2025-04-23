330ceac5505fafb1fae9505e4e1c400f66915c15
Shawn <changoloco.evg@gmail.com>
2025-04-23 17:02:37 +0000

    feat: add experimental barebones dashboard page

---
+++ src/routes/dashboard/+page.server.ts
@@ -0,0 +1,13 @@
+import type { ServiceData } from '@features/service/schema';
+import { ServiceController } from '@features/service/controller';
+import { error } from '@sveltejs/kit';
+
+export async function load(event) {
+  const { data, error: err } = await ServiceController.getMany(10);
+  
+  if (err) {
+    error(500, 'Error fetching services');
+  }
+
+  return { services: data as ServiceData[] };
+}
