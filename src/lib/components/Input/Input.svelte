c8e940918f1a4c94e66e45ce8836744b03d1d90f
Shawn <changoloco.evg@gmail.com>
2025-05-04 03:50:07 +0000

    feat(ui): add Input password type toggle

--- src/lib/components/Input/Input.svelte
+++ src/lib/components/Input/Input.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+	import { Eye } from 'lucide-svelte';
+
 	interface Properties {
 		name: string;
 		placeholder: string;
@@ -7,19 +9,41 @@
 	}
 
 	let { name, placeholder, type = 'text', value = $bindable(''), ...rest }: Properties = $props();
+	let localType = $state(type);
 
 	function handleInput(this: HTMLInputElement) {
 		value = this.value;
 	}
 </script>
 
-<input
-	class="h-12 px-4 $bg-ground-0 border $border-ground-2 rounded-lg $text-summit $placeholder"
-	{name}
-	{type}
-	{placeholder}
-	required
-	{value}
-	oninput={handleInput}
-	{...rest}
-/>
+{#snippet input()}
+	<input
+		class="w-full h-12 px-4 $bg-ground-0 border $border-ground-2 rounded-lg $text-summit $placeholder"
+		{name}
+		type={localType}
+		{placeholder}
+		required
+		{value}
+		oninput={handleInput}
+		{...rest}
+	/>
+{/snippet}
+
+{#if type === 'password'}
+	<div class="inline-flex items-center gap-2">
+		{@render input()}
+		<button
+			class="button button--side size-12"
+			aria-label="Mostrar Contraseña"
+			title="Mostrar Contraseña"
+			onclick={() => {
+				localType = localType === 'password' ? 'text' : 'password';
+			}}
+			type="button"
+		>
+			<Eye />
+		</button>
+	</div>
+{:else}
+	{@render input()}
+{/if}
